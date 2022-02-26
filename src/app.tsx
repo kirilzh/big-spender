ed416cd22866400e94dc9316b2cd75395bd5c878
Kiril Zhelyazkov <kiril.zhelyazkov9@gmail.com>
2022-02-26 11:19:09 +0200

    appbar

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,36 @@
-import Container from '@mui/material/Container'
-import Typography from '@mui/material/Typography'
+import { Fragment } from 'react';
+import AppBar from '@mui/material/AppBar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 
 export const App = () => {
     return (
-        <Container disableGutters maxWidth="lg">
+        <Fragment>
+            <CssBaseline />
+        <Container disableGutters maxWidth={false}>
+            <AppBar position="relative">
+                <Container maxWidth="lg" disableGutters>
+
+                <Toolbar>
+                    <Typography variant="h6" sx={{ flexGrow: 1 }}>
+                        BigSpender
+                    </Typography>
+
+                    <Button color="secondary" variant="contained">
+                        Login
+                    </Button>
+                </Toolbar>
+                </Container>
+            </AppBar>
+        <Container maxWidth="lg">
             <Typography variant="h4">
-                Hello with material ui
+                Static website
             </Typography>
         </Container>
+        </Container>
+        </Fragment>
     )
 }
