d87f75a011b06682d83a0cf7d7d11ad32842b851
Kiril Zhelyazkov <kiril.zhelyazkov9@gmail.com>
2022-02-26 14:00:03 +0200

    formatting and dynamodb

--- src/app.tsx
+++ src/app.tsx
@@ -7,30 +7,29 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 
 export const App = () => {
-    return (
-        <Fragment>
-            <CssBaseline />
-        <Container disableGutters maxWidth={false}>
-            <AppBar position="relative">
-                <Container maxWidth="lg" disableGutters>
+  return (
+    <Fragment>
+      <CssBaseline/>
+      <Container disableGutters maxWidth={false}>
+        <AppBar position="relative">
+          <Container maxWidth="lg" disableGutters>
+            <Toolbar>
+              <Typography variant="h6" sx={{ flexGrow: 1 }}>
+                BigSpender
+              </Typography>
 
-                <Toolbar>
-                    <Typography variant="h6" sx={{ flexGrow: 1 }}>
-                        BigSpender
-                    </Typography>
-
-                    <Button color="secondary" variant="contained">
-                        Login
-                    </Button>
-                </Toolbar>
-                </Container>
-            </AppBar>
+              <Button color="secondary" variant="contained">
+                Login
+              </Button>
+            </Toolbar>
+          </Container>
+        </AppBar>
         <Container maxWidth="lg">
-            <Typography variant="h4">
-                Static website
-            </Typography>
+          <Typography variant="h4">
+            Static website
+          </Typography>
         </Container>
-        </Container>
-        </Fragment>
-    )
+      </Container>
+    </Fragment>
+  )
 }
